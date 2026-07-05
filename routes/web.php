5ae45ac8d8e4d0ca14d3c77d546da6be1f00c2ee
Asus-004 <ferdialuthifaashma0112@gmail.com>
2026-07-05 13:23:41 +0700

    Mengubah tampilan welcome Laravel

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Halaman Utama: Jadwal Kuliah
 Route::get('/', function () {
-    return view('jadwal');
-})->name('jadwal');
-
-// Halaman Kedua: Daftar Tugas
-Route::get('/tugas', function () {
-    return view('tugas');
-})->name('tugas');
+    return view('welcome');
+})->name('welcome');
